5f5413980540ea70f007065582d2b496738e1fcc
Fredrick Kyalo <kyalomwendwa44@gmail.com>
2023-09-05 10:23:17 +0300

    Splashscreen.jpeg

---
+++ app/src/main/java/com/example/chatbot/Splashscreen1.java
@@ -0,0 +1,24 @@
+package com.example.chatbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen1);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen1.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
